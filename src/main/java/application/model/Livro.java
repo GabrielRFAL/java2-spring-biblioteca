9cdb9d8b0266e3e47fc035e712102e0af5d04d97
Gabriel Ribas Ferreira de Almeida <165100529+GabrielRFAL@users.noreply.github.com>
2025-10-17 19:42:27 +0000

    feat: criação views do  insert e list e Livro model

--- src/main/java/application/model/Livro.java
+++ src/main/java/application/model/Livro.java
@@ -1,24 +1,30 @@
 package application.model;
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Entity
 @Getter
 @Setter
-@Table(name = "generos")
+@Entity
 @NoArgsConstructor
-public class Genero {
+@Table(name = "livros")
+public class Livro {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
-    @Column(nullable = false, unique = true)
-    private String nome;
+    @Column(nullable = false)
+    private String titulo;
+
+    @ManyToOne
+    @JoinColumn(name = "id_genero")
+    private Genero genero;
 }
