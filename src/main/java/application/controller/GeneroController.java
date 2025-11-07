48a8ffedca6b578a0f34f7fc0dc99f3a1e9ef5da
Gabriel Ribas Ferreira de Almeida <165100529+GabrielRFAL@users.noreply.github.com>
2025-11-07 19:42:59 +0000

    feat: adição autores, livros

--- src/main/java/application/controller/GeneroController.java
+++ src/main/java/application/controller/GeneroController.java
@@ -10,13 +10,18 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import application.model.Genero;
+import application.model.Livro;
 import application.repository.GeneroRepository;
+import application.repository.LivroRepository;
 
 @Controller
 @RequestMapping("/generos")
 public class GeneroController {
     @Autowired
     private GeneroRepository generoRepo;
+
+    @Autowired
+    private LivroRepository livroRepo;
 
     @RequestMapping("/list")
     public String list(Model ui) {
@@ -36,47 +41,6 @@ public class GeneroController {
 
         generoRepo.save(genero);
 
-        return "redirect:/generos/list";
-    }
-    
-    @RequestMapping("/update")
-    public String update(@RequestParam("id") long id, Model ui) {
-        Optional<Genero> resultado = generoRepo.findById(id);
-
-        if (resultado.isEmpty()) {
-            return "redirect:/generos/list";
-        } 
-
-        ui.addAttribute("genero", resultado.get());
-        return "update";
-    }
-
-    @RequestMapping(value = "/update", method = RequestMethod.POST)
-    public String update(@RequestParam("id") long id, @RequestParam("genero") String nome) {
-        Optional<Genero> resultado = generoRepo.findById(id);
-        if (resultado.isPresent()) {
-            resultado.get().setNome(nome);
-            generoRepo.save(resultado.get());
-        }
-
-        return "redirect:/generos/list";
-    }
-
-    @RequestMapping("/delete")
-    public String delete(@RequestParam("id") long id, Model ui) {
-        Optional<Genero> resultado = generoRepo.findById(id);
-
-        if (resultado.isEmpty()) {
-            return "redirect:/generos/list";
-        }
-
-        ui.addAttribute("genero", resultado.get());
-        return "delete";
-    }
-
-    @RequestMapping(value = "/delete", method = RequestMethod.POST)
-    public String delete(@RequestParam("id") long id) {
-        generoRepo.deleteById(id);
         return "redirect:/generos/list";
     }
 }
